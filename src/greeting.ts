d317d0bc62dd97b799dc32fafe5991b798e0302f
Poppy <naphat.ana@pea.co.th>
2024-09-07 13:24:04 +0700

    update table

--- src/greeting.ts
+++ src/greeting.ts
@@ -1,22 +1,21 @@
 type GreetingProps = {
-    name: string;
-    age: number;
-    format?: string; 
-  };
-  
-  const greeting = ({ name, age, format }: GreetingProps) => {
-    if (format === "Table") {
-        console.table({ [name]: { value: age } });
-        return;
-    } else {
-        console.log(`Hi ${name}. Your format is ${format}`);
-        return;
-    }
-  };
-  
-  
-  greeting({ name: "Alice", age: 18, format: "Table" })
-  greeting({ name: "Bob", age: 18, format: "Default" })
-  greeting({ name: "Charlie", age: 18 }) // Hi, Charlie
-  greeting({ name: "Charlie", age: 18, format: "abc" }) // 🔴 typescript error, No console.log
-  
+  name: string;
+  age: number;
+  format?: string; 
+};
+
+const greeting = ({ name, age, format }: GreetingProps) => {
+  if (format === "Table") {
+      console.table({ ["name"]: { value: name },["age"]: { value: age } });
+      return;
+  } else {
+      console.log(`Hi ${name}. Your format is ${format}`);
+      return;
+  }
+};
+
+
+greeting({ name: "Alice", age: 18, format: "Table" })
+greeting({ name: "Bob", age: 18, format: "Default" })
+greeting({ name: "Charlie", age: 18 }) // Hi, Charlie
+greeting({ name: "Charlie", age: 18, format: "abc" }) // 🔴 typescript error, No console.log
